a35595a52a7729661d275cb355a078a92ae9f60e
rweisshaar1 <robbieweisshaar@gmail.com>
2023-09-06 09:17:51 -0500

    added resume, added redirect file

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css' ;
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
+import pdf from '../../src/assets/resume-2023-copy.pdf'
+
+
 
 export default function Resume () {
 
@@ -11,6 +13,9 @@ export default function Resume () {
       <h1>RESUME:</h1>
       <Card>
         <Card.Body>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, fugit. Quae, laudantium! Iure eum dolore inventore beatae cumque, dolor, obcaecati eaque eveniet saepe, ab earum nobis sunt labore porro nesciunt! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem reiciendis rerum, aspernatur ipsum quisquam harum nihil impedit obcaecati voluptas explicabo quo corporis cumque quia officiis dolorum? Cupiditate asperiores adipisci obcaecati?</Card.Body>
+        <div style={{ margin:'10px' }}>
+          <a href={pdf}>RESUME</a>
+        </div>
       </Card>
     </Container>
     </>
